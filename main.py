673e87670b8856b70a5055ba41dc7f82cd4edff1
sam curran <126618122+Samtheram13@users.noreply.github.com>
2023-02-28 22:05:18 +0000

    added option to enter ID and list songs

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 
 #gets auth for accessing spotify api
-sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id="9f77fac387634741a99fb710bc910565",
-                                                        client_secret="f2f248c7799e4b0fb34a91f960001974"))
+sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id="b8406ed4ca20481889faf98255ed51a9",
+                                                        client_secret="9507255eb988446c863fda4af6f9ee7e"))
 UN = input('User ID:')                                                   
 
 #list user playlists
@@ -11,3 +11,8 @@ dictPL = sp.user_playlists(user=UN, limit=50)
 
 for x in dictPL["items"]:
 	print(x["name"], x["id"])
+ 
+ID = input("Enter Playlist ID:")
+PI = sp.playlist_items(playlist_id=ID)
+
+
