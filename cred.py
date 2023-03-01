0209f0f75416526e178712a3e52009da5a7214d8
Alexander Wang <98280966+AlexanderWangY@users.noreply.github.com>
2023-03-01 15:34:44 +0000

    I added a user ID search and a way to select a playlist based on its name. It lists the title, the artist, and the length of each track in the playlist

---
+++ cred.py
@@ -0,0 +1,3 @@
+client_ID = 'b8406ed4ca20481889faf98255ed51a9'
+client_SECRET = '9507255eb988446c863fda4af6f9ee7e'
+redirect_url = 'http://localhost:9000'
--- main.py
+++ main.py
@@ -1,18 +1,54 @@
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
+import cred
 
-#gets auth for accessing spotify api
-sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id="b8406ed4ca20481889faf98255ed51a9",
-                                                        client_secret="9507255eb988446c863fda4af6f9ee7e"))
-UN = input('User ID:')                                                   
+#0o3133j3p3nrq81uoex50m50h
 
-#list user playlists
-dictPL = sp.user_playlists(user=UN, limit=50)
+
+def checkKey(dic, key):
+	if key in dic.keys():
+		return True
+	else:
+		return False
+
+
+sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(
+ client_id=cred.client_ID, client_secret=cred.client_SECRET))
+
+userID = input("Welcome to spotify playlist seacher, what is your user ID? \n")
+
+dictPL = sp.user_playlists(userID, limit=50)
+
+plDIC = {}
 
 for x in dictPL["items"]:
-	print(x["name"], x["id"])
- 
-ID = input("Enter Playlist ID:")
-PI = sp.playlist_items(playlist_id=ID)
+	plDIC[x["name"]] = x["id"]
 
+print("Your available playlists are:")
+for x in plDIC:
+	print("-" + x)
 
+print("-------------")
+
+while (True):
+	playlistSearch = input("Search for: ")
+	if (checkKey(plDIC, playlistSearch)):
+		break
+	else:
+		print("Could not be found, please try again")
+		continue
+
+# Select 4th index of plIDS for AlexWala Stream Music
+
+dictTracks = sp.user_playlist_tracks(user="0o3133j3p3nrq81uoex50m50h",
+                                     playlist_id=plDIC[playlistSearch],
+                                     limit=100)
+
+for y in dictTracks["items"]:
+	print("-----------------")
+	print(
+	 y["track"]["name"], "by:",
+	 y["track"]["artists"][0]["name"] + "\n" + "Length:",
+	 str((y["track"]["duration_ms"] // 1000) // 60) + ":" + str(
+	  ((y["track"]["duration_ms"]) // 1000) % 60).zfill(2))
+print("-----------------")
